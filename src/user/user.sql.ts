c9f131fe63057a26dc41f7abc0498b8bcba893ae
Chu-Jiwoo <smpts00@naver.com>
2022-08-05 01:56:40 +0900

    유저 상태 변경 기능

--- src/user/user.sql.ts
+++ src/user/user.sql.ts
@@ -1,15 +1,53 @@
-const SAVE_USER = `INSERT INTO 
+const SAVE_USER = `
+INSERT INTO 
 user(user_name , phone_number , name , password , birthday , register , user_status ,accept_date ,refresh_token) 
 VALUES( ? , ? , ? , ? , ? , ? , 'ACTIVE' , ? , ? );`;
 
-const FIND_USER_ALL = `SELECT user_id,user_name , phone_number , name , birthday , register , user_status, accept_date
+const GET_USER_ALL = `
+SELECT user_id,user_name , phone_number , name , birthday , register , user_status, accept_date
 FROM user;`;
-const FIND_USER_ID = `SELECT user_id,user_name , phone_number , name , birthday , register , user_status, accept_date
+const GET_USER_ID = `SELECT user_id,user_name , phone_number , name , birthday , register , user_status, accept_date
 FROM user
 WHERE user_id = ?;`;
 
-const SAVE_USER_KAKAO = `INSERT INTO 
+const SAVE_USER_KAKAO = `
+INSERT INTO 
 user(user_name , phone_number , name  , birthday , register , user_status ,accept_date ,refresh_token , social_id) 
 VALUES( ? , ? , ? ,  ? , ? , 'ACTIVE' , ? , ? , ? );`;
 
-export { SAVE_USER, FIND_USER_ALL, FIND_USER_ID, SAVE_USER_KAKAO };
+const UPDATE_USER_NAME = `
+UPDATE user
+SET user_name = ?
+WHERE user_id = ?
+`;
+
+const SAVE_USER_NAME_CHANGE = `
+INSERT INTO
+change_user_name(user_id_fk)
+VALUES(?)
+`;
+
+const GET_CHANGE_USER_NAME_COUNT = `
+SELECT *
+FROM change_user_name
+WHERE user_id_fk = ?
+AND
+timestampdiff(day,update_timestamp,current_timestamp) < 14;
+`;
+
+const UPDATE_USER_STATUS = `
+UPDATE user
+SET user_status = ?
+WHERE user_id = ?
+`;
+
+export {
+  SAVE_USER,
+  GET_USER_ALL,
+  GET_USER_ID,
+  SAVE_USER_KAKAO,
+  GET_CHANGE_USER_NAME_COUNT,
+  UPDATE_USER_NAME,
+  SAVE_USER_NAME_CHANGE,
+  UPDATE_USER_STATUS,
+};
